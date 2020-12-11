62eb0af07364dd504370b2a18e3bfac8682df388
maggie-cc <72293708+maggie-cc@users.noreply.github.com>
2020-12-11 08:50:34 -0500

    Add files via upload

--- artguide/index.js
+++ artguide/index.js
@@ -1,11 +1,11 @@
+//use express to build together the pages based on parts (layout and all the pages)
 const express = require('express'); //loading in the express package
-const layouts = require('express-ejs-layouts');
+const layouts = require('express-ejs-layouts');//load template engine
 const app = express(); //execute express and find the variable app
 
-const fs = require('fs');
 
-app.set('view engine', 'ejs');
-app.use(layouts);
+app.set('view engine', 'ejs'); //set the view engine to view as ejs
+app.use(layouts);//pass in ejs packet
 app.use(express.static(__dirname));
 
 //should a browser visit this 
@@ -13,6 +13,7 @@ app.get('/', (req, res)=>{
     res.render('index', {layout:'layout', title:'dsadfsdfhu'})
     
 }) 
+
 app.get('/pages.html', (req, res)=>{
     res.render('carousel', {layout:'layout'});
 }) 
